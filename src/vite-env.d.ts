8c82928b2ffbbece9e183c38fd5a7090d7dfcec7
Dmitrii Gorbunov <dmitriigorbunov1998@gmail.com>
2026-07-16 21:59:51 +0300

    (UDS): types/added-ImportMetaEnv-types-for-VITE_variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,14 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  readonly VITE_EDS_LOGIN: string;
+  readonly VITE_EDS_PASSWORD: string;
+  readonly VITE_SUPABASE_URL?: string;
+  readonly VITE_SUPABASE_ANON_KEY?: string;
+  readonly VITE_APP_TITLE?: string;
+  readonly VITE_APP_ENV?: string;
   readonly VITE_API_URL: string;
   readonly VITE_API_KEY: string;
-  readonly VITE_SUPABASE_URL: string;
-  readonly VITE_SUPABASE_ANON_KEY: string;
 }
 
 interface ImportMeta {
