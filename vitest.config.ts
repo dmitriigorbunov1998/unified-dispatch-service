0f669ab327643010e6b07b1ef1dfa2eebaad195e
Dmitrii Gorbunov <dmitriigorbunov1998@gmail.com>
2026-07-22 08:53:07 +0300

    (UDS): chore/added-merge-config-for-vite-and-vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,29 +1,34 @@
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
+import { defineConfig, mergeConfig } from 'vitest/config';
 
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    // vitest.config.ts — для unit и компонентных тестов
-    globals: true,                        // describe, it, expect доступны без импорта
-    environment: 'jsdom',                 // Эмуляция браузера (DOM API)
-    setupFiles: ['./src/test/setup.ts'],  // Настройки перед тестами
-    exclude: [
-      'node_modules',
-      'dist',
-      'e2e',                              // ← Исключаем e2e тесты
-      '**/e2e/**',                        // ← На всякий случай
-    ],                                    // НЕ запускать E2E тесты через Vitest
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
+import viteConfig from './vite.config';
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['./src/test/setup.ts'],
+
       exclude: [
-        'node_modules/',
-        'src/test/',
-        '**/*.d.ts',
-        '**/*.config.*',
-        '**/mockData/*',
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/e2e/**',
+        '**/tests/e2e/**',
       ],
+
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'json', 'html'],
+
+        exclude: [
+          '**/node_modules/**',
+          'src/test/**',
+          '**/*.d.ts',
+          '**/*.config.*',
+          '**/mockData/**',
+        ],
+      },
     },
-  },
-});
+  })
+);
