11bc6c95fba7b29f1b3af1c3e76ca4af0c1a7489
Dmitrii Gorbunov <dmitriigorbunov1998@gmail.com>
2026-07-26 23:02:08 +0300

    (UDS): chore/configure-TypeScript--server-runtime

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import path from 'node:path';
 
-// https://vite.dev/config/
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
 export default defineConfig({
   plugins: [react()],
 
